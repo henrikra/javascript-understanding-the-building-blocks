1ec611e412464766dc1a1dcf4b745d6108c19be9
Sami H <samih90@gmail.com>
2018-11-13 23:31:59 +0200

    Formatting some more

--- answers/myMap.js
+++ answers/myMap.js
@@ -1,10 +1,10 @@
 const myMap = (myArray, callback) => {
   let result = [];
   for (let i = 0; i < myArray.length; i++) {
-      var x = callback(myArray[i]);
-      result.push(x);
-};
-return result;
+    var x = callback(myArray[i]);
+    result.push(x);
+  };
+  return result;
 }
 
 module.exports = myMap;
